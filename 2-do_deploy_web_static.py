7fbf41a0bacce979b175114a8481031e97da622d
mohamed gharieb <m.gharieb89@gmail.com>
2024-03-10 13:57:30 -0700

    t2 v2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,6 +6,7 @@
 from datetime import datetime
 from os.path import exists
 from fabric.api import *
+env.hosts = ['100.26.219.106', '35.153.16.107']
 
 
 def do_deploy(archive_path):
@@ -22,12 +23,14 @@ def do_deploy(archive_path):
         direct = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
         run('mkdir -p {}{}/'.format(direct, extension_file))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(name_file, direct, extension_file))
+        run('tar -xzf /tmp/{} -C {}{}/'
+            .format(name_file, direct, extension_file))
         run('rm /tmp/{}'.format(name_file))
         run('mv {0}{1}/web_static/* {0}{1}/'.format(direct, extension_file))
         run('rm -rf {}{}/web_static'.format(direct, extension_file))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(direct, extension_file))
+        run('ln -s {}{}/ /data/web_static/current'
+            .format(direct, extension_file))
         return True
     except:
         return False
