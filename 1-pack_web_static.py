168920f8168c8121acbc6eb8b2c852d981203017
mohamed gharieb <m.gharieb89@gmail.com>
2024-03-09 22:50:32 -0800

    t1 v1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -12,10 +12,10 @@ def do_pack():
     function for archive all content
     """
     time_now = datetime.now()
-    tarball_archive = 'web_static_' + time_now.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
+    tarballnm = 'web_static_' + time_now.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
     local('mkdir -p versions')
-    create_tar = local('tar -cvzf versions/{} web_static'.format(tarball_archive))
+    create_tar = local('tar -cvzf versions/{} web_static'.format(tarballnm))
     if create_tar is not None:
-        return tarball_archive
+        return tarballnm
     else:
         return None
