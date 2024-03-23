8864ccfa275e20aac3e2c56651a886060f9de2a3
mohamed gharieb <m.gharieb89@gmail.com>
2024-03-23 16:39:45 -0700

    t2 v2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -16,9 +16,11 @@ def hello_world():
 def hbnb_flask():
     return "HBNB"
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def text_flask(text):
     return "C" + text.replace("_", " ")
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
