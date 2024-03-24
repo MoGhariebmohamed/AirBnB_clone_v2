35b47a8ea82e7b298ac9280c89d65690474286cb
mohamed gharieb <m.gharieb89@gmail.com>
2024-03-23 17:34:08 -0700

    t5 v2

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -37,7 +37,7 @@ def pythonint_flask(n):
     return "{:d} is a number".format(n)
 
 
-@app.route('/number_template/<n>', strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def html_flask(n):
     return  render_template('5-number.html', n=n)
 
