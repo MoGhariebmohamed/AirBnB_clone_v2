f7ef17f4eb827646fbac957bd8cefa7c535b116f
mohamed gharieb <m.gharieb89@gmail.com>
2024-04-03 03:57:06 -0700

    t5v6

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 """ new class for sqlAlchemy """
-from os import getenv
-import sqlalchemy
-from sqlalchemy.orm import sessionmaker, scoped_session
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import Base
-from models.state import State
+import models
+from models.amenity import Amenity
+from models.base_model import BaseModel, Base
 from models.city import City
-from models.user import User
 from models.place import Place
 from models.review import Review
-from models.amenity import Amenity
-
+from models.state import State
+from models.user import User
+from os import getenv
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 class DBStorage:
     """ create tables in environmental"""
@@ -77,7 +76,7 @@ class DBStorage:
         Base.metadata.create_all(self.__engine)
         sec = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(sec)
-        self.__session = Session()
+        self.__session = Session
 
     def close(self):
         """ calls remove()
